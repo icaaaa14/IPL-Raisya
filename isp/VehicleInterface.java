cb0fb13a8ba33f6a7f34944751903a9ede4219a6
Raisyaf <rfebiyane@gmail.com>
2023-10-26 10:48:35 +0700

    isp

---
+++ isp/VehicleInterface.java
@@ -0,0 +1,9 @@
+interface VehicleInterface {
+    void drive();
+
+    void stop();
+
+    void refuel();
+
+    void openDoors();
+}
---
+++ openclose/StudentCinema.java
@@ -0,0 +1,10 @@
+class StudentCinema extends Cinema {
+    public StudentCinema(double price) {
+        this.price = price;
+    }
+
+    @Override
+    Double calculateAdminFee() {
+        return price * 2 / 100;
+    }
+}
